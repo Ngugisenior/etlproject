d6536005d6b2631dff0fc87ffd1beac9a06bfe46
Ngugisenior <ngugijoseph992@gmail.com>
2022-03-02 23:44:42 +0100

    2022-03-02 23:44:42.543746

--- create_tables.py
+++ create_tables.py
@@ -1,6 +1,7 @@
 from commit import cmd
 from engine import Base, engine
 from constructors import Constructor
+from drivers import Driver
 
 if __name__ == "__main__":
     Base.metadata.create_all(engine)
