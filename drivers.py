0d0f28eccc3200225b6ef28d9ac221ef7d80294b
Ngugisenior <ngugijoseph992@gmail.com>
2022-03-02 23:41:40 +0100

    2022-03-02 23:41:40.092901

--- drivers.py
+++ drivers.py
@@ -3,13 +3,13 @@ from sqlalchemy import create_engine, Column, Integer, String, Date
 from engine import Base
 from commit import cmd
 
-class Constructor(Base):
-    __tablename__ = 'constructor'
+class Driver(Base):
+    __tablename__ = 'driver'
     driverId = Column(String(255), primary_key=True)
     url = Column(String(255))
     givenName = Column(String(255))
     familyName = Column(String(255))
-    dateOfBirth = Column(String(255))
+    dateOfBirth = Column(Date)
     nationality = Column(String(255))
 
 os.system(cmd)
